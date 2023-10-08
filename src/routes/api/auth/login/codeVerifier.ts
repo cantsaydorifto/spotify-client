06ec4c6eac026a6ba84509880ee91cc508171c77
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-10-09 05:10:13 +0530

    add packages, function for verifier and challenge

---
+++ src/routes/api/auth/login/codeVerifier.ts
@@ -0,0 +1,48 @@
+import { SHA256 } from 'crypto-js';
+import { enc } from 'crypto-js';
+import { randomBuffer } from 'secure-random';
+
+const mask = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~';
+
+export function random(size: number): string {
+  let value = '';
+
+  const bytes = randomBuffer(size);
+  const scale = 256 / mask.length;
+
+  for (let i = 0; i < size; i++) {
+    value += mask.charAt(Math.floor(bytes[i] / scale));
+  }
+
+  return value;
+}
+
+function hash(str: string): string {
+  return enc.Base64.stringify(SHA256(str));
+}
+
+function base64url(str: string): string {
+  return str.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+}
+
+function createVerifier(length: number = 128): string {
+  if (length < 43 || length > 128) {
+    throw new Error(`expected length ${length} between 43 and 128`);
+  }
+
+  return random(length);
+}
+
+function createChallenge(verifier: string): string {
+  return base64url(hash(verifier));
+}
+
+export function createChallengeAndVerifier(length: number = 128) {
+  const verifier = createVerifier(length);
+  const challenge = createChallenge(verifier);
+
+  return {
+    codeVerifier: verifier,
+    codeChallenge: challenge
+  };
+}
