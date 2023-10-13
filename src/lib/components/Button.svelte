e80a73611350dab7157e2c410f211509f76c30b9
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-10-13 20:37:24 +0530

    add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
+  type ElementType = $$Generic<'button' | 'a'>;
+  interface ButtonElements {
+    button: HTMLButtonAttributes;
+    a: HTMLAnchorAttributes;
+  }
+  type $$Props = ButtonElements[ElementType] & {
+    element: ElementType;
+    style: 'solid' | 'outline' | 'danger';
+    styleClasses?: string;
+  };
+  export let element: 'button' | 'a';
+  export let style: 'solid' | 'outline' | 'danger';
+  export const styleClasses = '';
+</script>
+
+<svelte:element this={element} class="button {style} {styleClasses}" {...$$restProps}>
+  <slot />
+</svelte:element>
+
+<style>
+  .button {
+    border: none;
+    font-weight: 600;
+    font-size: 1rem;
+    border-radius: 20px;
+    cursor: pointer;
+    text-decoration: none;
+    padding: 7px 15px;
+    transition: all 0.3s;
+  }
+  .solid {
+    background-color: var(--accent-color);
+    color: black;
+    border: 2px solid var(--accent-color);
+  }
+  .outline {
+    background: none;
+    color: var(--text-color);
+    border: 2px solid;
+  }
+  .danger {
+    background-color: var(--error);
+    color: white;
+    border: 2px solid var(--error);
+  }
+  .solid:hover,
+  .danger:hover {
+    filter: brightness(0.9);
+  }
+  .outline:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+</style>
