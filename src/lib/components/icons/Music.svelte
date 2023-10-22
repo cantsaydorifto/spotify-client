c83e03cea6e372a604dca87ea34f36105fac7449
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-10-23 00:42:37 +0530

    add some icons

---
+++ src/lib/components/icons/Music.svelte
@@ -0,0 +1,13 @@
+<svg
+  {...$$restProps}
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M9 18V5l12-2v13" />
+  <circle cx="6" cy="18" r="3" />
+  <circle cx="18" cy="16" r="3" />
+</svg>
---
+++ src/lib/components/icons/Play.svelte
@@ -0,0 +1,11 @@
+<svg aria-hidden="true" width="25" height="25" viewBox="0 0 24 24">
+  <path
+    d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"
+  />
+</svg>
+
+<style>
+  svg {
+    display: block;
+  }
+</style>
