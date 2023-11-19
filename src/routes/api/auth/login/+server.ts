951353caf0c5633b8708e4a05417c52c1b71f560
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-11-20 00:23:38 +0530

    change scope

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -3,7 +3,7 @@ import { BASE_URL, CLIENT_ID } from '$env/static/private';
 import { createChallengeAndVerifier } from './codeVerifier';
 
 const scope =
-  'ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private';
+  'playlist-read-private playlist-read-collaborative user-top-read user-library-read user-read-email';
 
 export async function GET({ cookies }) {
   const { codeChallenge, codeVerifier } = createChallengeAndVerifier();
